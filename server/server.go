2e7d6bdb6ff7094aef58a1a9d85b569b4bee1ece
Jeroen Leuenberger <jereileu@proton.me>
2023-10-12 18:09:25 +0200

    Modify function in server.go to check method
    Modified the http.HandleFunc function in Run() in server.go to detect the http method used in the http request

--- server/server.go
+++ server/server.go
@@ -8,49 +8,57 @@ import (
 	"strings"
 )
 
-func main() {
-	config, err := fs.Config()
-	if err != nil {
-		return
-	}
+func Run(config fs.Conf) {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		url := r.URL.Path
-		if len(url) > 1 {
-			path := strings.Split(url, "/")
-			db := path[1]
-			if len(path) > 2 {
-				// table := path[2]
-			} else {
-				tables, tblErr := getTables(db, config.RootDir)
-				if tblErr != nil {
-					log.Print(tblErr)
-					w.WriteHeader(500)
-				} else {
-					respErr := sendJson(tables, w)
-					if respErr != nil {
-						log.Print(respErr)
-						w.WriteHeader(500)
-					}
-				}
-			}
+		if r.Method == "GET" || r.Method == "" {
+			get(w, r, config)
+		} else if r.Method == "POST" {
+			// post(w, config)
+		} else if r.Method == "DELETE" {
+			// delete(w, config)
 		} else {
-			dbs, dbErr := getDBs(config.RootDir)
-			if dbErr != nil {
-				log.Print(dbErr)
-				w.WriteHeader(500)
-			} else {
-				respErr := sendJson(dbs, w)
-				if respErr != nil {
-					log.Print(respErr)
-					w.WriteHeader(500)
-				}
-			}
+			w.WriteHeader(405)
 		}
 	})
 	if config.HTTPSMode {
 		log.Fatal(http.ListenAndServeTLS(config.Port, config.SSLCert, config.SSLKey, nil))
 	} else {
 		log.Fatal(http.ListenAndServe(config.Port, nil))
+	}
+}
+
+func get(w http.ResponseWriter, r *http.Request, config fs.Conf) {
+	url := r.URL.Path
+	if len(url) > 1 {
+		path := strings.Split(url, "/")
+		db := path[1]
+		if len(path) > 2 {
+			// table := path[2]
+		} else {
+			tables, tblErr := getTables(db, config.RootDir)
+			if tblErr != nil {
+				log.Print(tblErr)
+				w.WriteHeader(500)
+			} else {
+				respErr := sendJson(tables, w)
+				if respErr != nil {
+					log.Print(respErr)
+					w.WriteHeader(500)
+				}
+			}
+		}
+	} else {
+		dbs, dbErr := getDBs(config.RootDir)
+		if dbErr != nil {
+			log.Print(dbErr)
+			w.WriteHeader(500)
+		} else {
+			respErr := sendJson(dbs, w)
+			if respErr != nil {
+				log.Print(respErr)
+				w.WriteHeader(500)
+			}
+		}
 	}
 }
 
