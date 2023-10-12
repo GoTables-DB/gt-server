102ed18b0cd3ad51f4f1ad4a7a0a31e9dcac5298
Jeroen Leuenberger <jereileu@proton.me>
2023-10-12 21:33:01 +0200

    Launch server when running main.go
    Added code in main() in main.go to start the GoTables server

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"github.com/Jero075/gotables/fs"
+	"github.com/Jero075/gotables/server"
+	"log"
+)
 
+func main() {
+	config, err := fs.Config()
+	if err != nil {
+		return
+	}
+	go server.Run(config)
+	if config.HTTPSMode {
+		log.Println("Started server at " + "https://127.0.0.1" + config.Port)
+	} else {
+		log.Println("Started server at " + "http://127.0.0.1" + config.Port)
+	}
+	// log.Println("Logs are stored in " + config.LogDir)
+	log.Println("Press 'ctrl' + 'C' to stop this program")
+	for {
+
+	}
 }
