549bbec06c674e7786a165de4dbc513bf6f631f6
Jeroen Leuenberger <jereileu@proton.me>
2023-10-14 16:38:58 +0200

    Add url() function
    Added the url() function in server.go to take care of the requested resource. This tidies up the functions for the different http methods used and fixes the error of http(s)://ip/db/ with a trailing / returning a 404 error.

--- server/server.go
+++ server/server.go
@@ -8,6 +8,10 @@ import (
 	"strings"
 )
 
+type Post struct {
+	Name string `json:"name"`
+}
+
 func Run(config fs.Conf) {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "GET" || r.Method == "" {
@@ -28,12 +32,33 @@ func Run(config fs.Conf) {
 }
 
 func get(w http.ResponseWriter, r *http.Request, config fs.Conf) {
-	url := r.URL.Path
-	if len(url) > 1 {
-		path := strings.Split(url, "/")
-		db := path[1]
-		if len(path) > 2 {
-			table := path[2]
+	db, table := url(r)
+	if db == "" {
+		dbs, dbErr := getDBs(config.RootDir)
+		if dbErr != nil {
+			log.Println(dbErr)
+			w.WriteHeader(500)
+		} else {
+			respErr := sendJson(dbs, w)
+			if respErr != nil {
+				log.Println(respErr)
+				w.WriteHeader(500)
+			}
+		}
+	} else {
+		if table == "" {
+			tables, tblErr := getTables(db, config.RootDir)
+			if tblErr != nil {
+				log.Println(tblErr)
+				w.WriteHeader(404)
+			} else {
+				respErr := sendJson(tables, w)
+				if respErr != nil {
+					log.Println(respErr)
+					w.WriteHeader(500)
+				}
+			}
+		} else {
 			tbl, err, status404 := fs.GetTable(db, table, config.RootDir)
 			if err != nil {
 				log.Println(err)
@@ -49,30 +74,6 @@ func get(w http.ResponseWriter, r *http.Request, config fs.Conf) {
 					w.WriteHeader(500)
 				}
 			}
-		} else {
-			tables, tblErr := getTables(db, config.RootDir)
-			if tblErr != nil {
-				log.Println(tblErr)
-				w.WriteHeader(404)
-			} else {
-				respErr := sendJson(tables, w)
-				if respErr != nil {
-					log.Println(respErr)
-					w.WriteHeader(500)
-				}
-			}
-		}
-	} else {
-		dbs, dbErr := getDBs(config.RootDir)
-		if dbErr != nil {
-			log.Println(dbErr)
-			w.WriteHeader(500)
-		} else {
-			respErr := sendJson(dbs, w)
-			if respErr != nil {
-				log.Println(respErr)
-				w.WriteHeader(500)
-			}
 		}
 	}
 }
@@ -83,6 +84,29 @@ func post(w http.ResponseWriter, r *http.Request, config fs.Conf) {
 
 func del(w http.ResponseWriter, r *http.Request, config fs.Conf) {
 	w.WriteHeader(405)
+}
+
+func url(r *http.Request) (db string, table string) {
+	path := r.URL.EscapedPath()
+	splitPath := strings.Split(path, "/")
+	var splitPathNoEmpty []string
+	for _, element := range splitPath {
+		if element != "" {
+			splitPathNoEmpty = append(splitPathNoEmpty, element)
+		}
+	}
+	if len(splitPathNoEmpty) > 0 {
+		db = splitPathNoEmpty[0]
+		if len(splitPathNoEmpty) > 1 {
+			table = splitPathNoEmpty[1]
+		} else {
+			table = ""
+		}
+	} else {
+		db = ""
+		table = ""
+	}
+	return db, table
 }
 
 func getDBs(dir string) ([]string, error) {
